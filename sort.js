1ac3461802146c60ea4d4f309114961cebf412a3
Nonn Adam <nonn.adiko@gmail.com>
2016-08-05 15:49:27 +0300

    arrayToList function introduced

--- sort.js
+++ sort.js
@@ -7,7 +7,10 @@ function add() {
 			return a - b
 		}
 	);
-	document.getElementById("myArray").innerHTML = arr;
+//	document.getElementById("myArray").innerHTML = arr;
+	document.getElementById("myArray").innerHTML = arrayToList(arr);
+	emptyTextBox();
+	document.getElementById("textBox").focus();
 };
 
 function empty() {
@@ -22,4 +25,14 @@ function emptyArray() {
 
 function emptyTextBox() {
 	document.getElementById("textBox").value = "";
+};
+
+function arrayToList(arr) {
+	var retVal = "";
+	retVal = "<ul>"
+	for (i = 0; i < arr.length; i++) {
+		retVal += "<li>" + arr[i] + "</li>"
+	}
+	retVal += "</ul>";
+	return retVal;
 };
